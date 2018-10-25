f65d2c1dad4c80075a65ff02b572526901f44ad8
SafirAchour <achoursaphir@gmail.com>
2018-10-25 17:06:06 +0200

    Commit Newly created interactive DB

--- PHP/tagBeSill/public/index.php
+++ PHP/tagBeSill/public/index.php
@@ -1,3 +1,27 @@
 <?php 
-include __DIR__ . '/../model/articles.php';
-include __DIR__ . '/../view/ArticleList.php';
+
+$config = include __DIR__ . '/../config/config.php';
+require __DIR__ . '/../model/Project.php';  // <=   ???
+
+try {
+    $projects = getAllProjects();
+} catch (Exception $e) {
+    echo 'An error occured with code : ' .$e->getMessage();
+    exit;
+}
+
+var_dump($projects->fetchAll());
+
+/*
+-> go in config and return config
+-> require project.php
+    ->  require connection.php
+        -> create a connection
+    -> define getAllProjects function
+-> call getAllProjects
+    -> send a query
+    -> return the result
+*/
+
+    
+    
--- PHP/tagBeSill/v1.sql
+++ PHP/tagBeSill/v1.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `Status`(
     description BLOB NOT NULL
 ) engine=InnoDB;
     
-CREATE TABLE IF NOT EXISTS Project (
+CREATE TABLE IF NOT EXISTS Project(
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description BLOB NOT NULL,
@@ -33,6 +33,28 @@ CREATE TABLE IF NOT EXISTS ProjectCategory(
     FOREIGN KEY (categoryId) REFERENCES Category(id)
 ) ENGINE=InnoDB;
 
+/* INSERTING VALUES TO DISPLAY SOMETHING */
 
+INSERT INTO category(label, description) VALUES
+	('Management', 'Errare Humanum est, sed perseverare diabolicum'),
+	('Not Management', 'Alea iacta est'),
+	('Anti-Management', 'Didjabooti');
     
+
+INSERT INTO Status(label, description) VALUES
+	('Analysis', 'Errare Humanum est, sed perseverare diabolicum'),
+	('In progress', 'Alea iacta est'),
+	('Blocked', 'Didjabooti'),
+	('Out of budget', 'Didjabooti');
+    
+INSERT INTO Project(title, description, image, publishingDate, statusId) VALUES
+	('wf3pm', 'Errare Humanum est, sed perseverare diabolicum', 'img/myPictures1.png', NOW(), 1),
+	('wf3pm', 'boom', 'img/myPictures2.png', NOW(), 3);
+
+INSERT INTO ProjectCategory VALUES
+	(1,1),
+	(2,2),
+	(2,3);
+    
+
     
